56ef1854dc6b88811d19e3fe0f2f48f6568c1422
kz-17 <s111310533@student.nqu.edu.tw>
2025-10-26 14:24:39 +0800

    Update 1.py

--- HW1/1.py
+++ HW1/1.py
@@ -1,1 +1,16 @@
 
+import sympy as sp
+
+def f(x):
+    return x**3
+
+def df(expr, var):
+    return sp.diff(expr, var)
+
+def integratal(expr, x, a, b):
+    return sp.integrate(expr, (x, a, b))
+
+if __name__ == '__main__':
+    x = sp.Symbol('x')
+    print('diff:', df(f(x), x))
+    print('integratal: ', integratal(f(x), x, 0, 3))
